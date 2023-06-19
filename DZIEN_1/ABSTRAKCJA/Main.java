42b28d3682e64fa9562e22a31fc5353368191996
albim72 <marcin.albiniak72@gmail.com>
2023-06-19 10:57:29 +0200

    Create Main.java

---
+++ DZIEN_1/ABSTRAKCJA/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MojaKlasa mk = new MojaKlasa(6,9,11);
+        System.out.println(mk.opis());
+        System.out.println(mk.policz());
+        mk.msg();
+    }
+}
