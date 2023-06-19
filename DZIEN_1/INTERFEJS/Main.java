75a1eb4bb73e452cbb9d6310b78120d2065e478a
albim72 <marcin.albiniak72@gmail.com>
2023-06-19 11:43:27 +0200

    Create Main.java

---
+++ DZIEN_1/INTERFEJS/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Users usr = new Users();
+        usr.info();
+
+        System.out.printf("Rok: %d\n",IEkstra.rok);
+        usr.opis(2300,0.88);
+        System.out.println(usr.user("Jan","Kot"));
+        usr.sekcja();
+    }
+}
