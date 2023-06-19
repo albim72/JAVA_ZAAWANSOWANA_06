9283bbaf24c0b306f859eb43ed7a2aacc5d45e40
albim72 <marcin.albiniak72@gmail.com>
2023-06-19 11:16:55 +0200

    Update MojaKlasa.java

--- DZIEN_1/ABSTRAKCJA/MojaKlasa.java
+++ DZIEN_1/ABSTRAKCJA/MojaKlasa.java
@@ -1,12 +1,31 @@
 public class MojaKlasa extends Prototyp {
     int s;
+    int k;
     public MojaKlasa(int x, int y, int s) {
         super(x, y);
         this.s = s;
+
     }
 
+    public MojaKlasa(int x, int y, int s, int k, Dodatek dod) {
+        super(x, y);
+        this.s = s;
+        this.k = k;
+        this.dod = dod;
+    }
+
+    Dodatek dod = new Dodatek(67);
     @Override
     public int policz() {
         return (x+y)*s;
+    }
+
+    @Override
+    public void msg() {
+        System.out.println("to jest ważna wiadmość!!!");
+    }
+
+    public int wynik_dod(){
+        return dod.fx();
     }
 }
