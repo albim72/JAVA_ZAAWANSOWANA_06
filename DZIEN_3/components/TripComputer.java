2b10a02e07b2dae73eb5e48cc9277efc5b556dac
albim72 <marcin.albiniak72@gmail.com>
2023-06-20 15:59:52 +0200

    Create TripComputer.java

---
+++ DZIEN_3/components/TripComputer.java
@@ -0,0 +1,28 @@
+package marcin.com.components;
+
+import marcin.com.cars.Car;
+
+public class TripComputer {
+    private Car car;
+
+    public TripComputer() {
+
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void showFuelLevel(){
+        System.out.println("Poziom paliwa: " + this.car.getFuel());
+    }
+
+    public void ShowStatus(){
+        if(this.car.getEngine().isStarted()){
+            System.out.println("Samochód jest uruchomiony");
+        }
+        else {
+            System.out.println("Samochód nie jest uruchomiony!");
+        }
+    }
+}
