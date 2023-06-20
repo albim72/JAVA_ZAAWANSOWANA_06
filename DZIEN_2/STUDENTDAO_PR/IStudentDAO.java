1e57bfb8505f20baf3aebcd8fec41d5623fcff85
albim72 <marcin.albiniak72@gmail.com>
2023-06-20 11:45:02 +0200

    Create IStudentDAO.java

---
+++ DZIEN_2/STUDENTDAO_PR/IStudentDAO.java
@@ -0,0 +1,10 @@
+package marcin.com;
+
+import java.util.List;
+
+public interface IStudentDAO {
+    public List<Student> getAllStudent();
+    public Student getStudent(int nralb);
+    public void updateStudent(Student student);
+    public void deleteStudent(Student student);
+}
