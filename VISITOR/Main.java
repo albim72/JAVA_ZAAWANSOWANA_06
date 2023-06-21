5d948fd5380c81abeb333abb531397af8b6ea867
albim72 <marcin.albiniak72@gmail.com>
2023-06-21 11:20:01 +0200

    Create Main.java

---
+++ VISITOR/Main.java
@@ -0,0 +1,28 @@
+package marcin.com;
+
+import marcin.com.shapes.*;
+import marcin.com.visitor.XMLExportVisitor;
+public class Main {
+    public static void main(String[] args) {
+
+        Dot dot = new Dot(1,10,55);
+        Cicrle circle = new Cicrle(2,23,15,10);
+        Rectangle rectangle = new Rectangle(3,10,17,20,30);
+
+        CompoundShape compoundShape = new CompoundShape(4);
+        compoundShape.add(dot);
+        compoundShape.add(circle);
+        compoundShape.add(rectangle);
+
+        CompoundShape c = new CompoundShape(5);
+        c.add(dot);
+        compoundShape.add(c);
+
+        export(circle,compoundShape);
+    }
+    
+    private static void export(Shape... shapes){
+        XMLExportVisitor exportVisitor = new XMLExportVisitor();
+        System.out.println(exportVisitor.export(shapes));
+    }
+}
