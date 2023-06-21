eddbce050cb2c178ddd0d65be850b598284f12d8
albim72 <marcin.albiniak72@gmail.com>
2023-06-21 15:11:09 +0200

    Create socket_conn.java

---
+++ socket_conn.java
@@ -0,0 +1,26 @@
+package marcin.com;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int c;
+        Socket s = new Socket("whois.internic.net",43);
+
+        InputStream in = s.getInputStream();
+        OutputStream out = s.getOutputStream();
+
+        String str = (args.length == 0 ? "OraclePressBooks.com":args[0]) + "\n";
+
+        byte buf[] = str.getBytes();
+        out.write(buf);
+
+        while ((c=in.read()) != -1){
+            System.out.println((char) c);
+        }
+        s.close();
+    }
+}
